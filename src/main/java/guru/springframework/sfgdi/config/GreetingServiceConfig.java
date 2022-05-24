e5acc08cbdd994fb70198d8e1c35d43d122c687f
Robert Zaranek <robert.zaranek@bilansjr.pl>
2022-05-24 19:43:52 +0200

    application properties

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -2,50 +2,80 @@ package guru.springframework.sfgdi.config;
 
 import com.springframework.pets.PetService;
 import com.springframework.pets.PetServiceFactory;
+import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
+@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
     @Bean
-    PetServiceFactory petServiceFactory(){ return new PetServiceFactory(); }
-    @Profile({"dog", "default"})
-    @Bean
-    PetService dogPetService(PetServiceFactory petServiceFactory){
-        return petServiceFactory.getPetService("dog");
+    FakeDataSource fakeDataSource(@Value("${guru.username}") String username,
+                                  @Value("${guru.password}") String password,
+                                  @Value("${guru.jdbcurl}") String jdbcurl) {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcurl(jdbcurl);
+
+        return fakeDataSource;
     }
-    @Profile({"cat"})
-    @Bean
-    PetService catPetService(PetServiceFactory petServiceFactory){
-        return petServiceFactory.getPetService("cat");
-    }
-    @Profile({"ES", "default"})
-    @Bean("i18nService")
-    I18NSpanishService i18NSpanishService(){ return new I18NSpanishService(); }
 
     @Bean
-    EnglishGreetingRepository englishGreetingRepository(){
+    PetServiceFactory petServiceFactory() {
+        return new PetServiceFactory();
+    }
+
+    @Profile({"dog", "default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("dog");
+    }
+
+    @Profile({"cat"})
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("cat");
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18NSpanishService i18NSpanishService() {
+        return new I18NSpanishService();
+    }
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository() {
         return new EnglishGreetingRepositoryImpl();
     }
+
     @Profile("EN")
     @Bean
-    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository){
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository) {
         return new I18nEnglishGreetingService(englishGreetingRepository);
     }
+
     @Primary
     @Bean
-    PrimaryGreetingService primaryGreetingService(){ return new PrimaryGreetingService(); }
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
 //    @Bean - I put configuration to xml file sfgdi-config.xml
 //    ConstructorGreetingService constructorGreetingService(){ return new ConstructorGreetingService(); }
 
     @Bean
-    PropertyInjectedGreetingService propertyInjectedGreetingService(){ return new PropertyInjectedGreetingService(); }
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
 
     @Bean
-    SetterInjectedGreetingService setterInjectedGreetingService(){ return new SetterInjectedGreetingService(); }
+    SetterInjectedGreetingService setterInjectedGreetingService() {
+        return new SetterInjectedGreetingService();
+    }
 }
